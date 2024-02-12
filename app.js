cccb112be48ddfe289b4d41dda18cd886b7202bf
Mike Ramos <michaelangeloram8@gmail.com>
2024-02-12 16:48:22 -0600

    newest commit

--- app.js
+++ app.js
@@ -8,6 +8,15 @@ const app = express();
 
 app.use(bodyParser.json()); // x-www-form-urlencodedb (default) but here we want json()
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,  POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+});
+
 app.use("/feed", feedRoutes);
 
 app.listen(8080);
