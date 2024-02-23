46588fe1747009d98901a0efa30485be8757ca36
Mike Ramos <michaelangeloram8@gmail.com>
2024-02-23 08:44:56 -0600

    new commits

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -1,4 +1,5 @@
 import React, { Component, Fragment } from "react";
+import openSocket from "socket.io-client";
 
 import Post from "../../components/Feed/Post/Post";
 import Button from "../../components/Button/Button";
@@ -35,6 +36,10 @@ class Feed extends Component {
       .catch(this.catchError);
 
     this.loadPosts();
+    openSocket("http://localhost:8080");
+    console.log(
+      "socket.io is open for connection and ready to connect with server."
+    );
   }
 
   loadPosts = (direction) => {
